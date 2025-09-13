8b33e3d3252cc2222fbc177726282b810d9213b5
sanjay bharvad <sanjaybharvad1312@gmail.com>
2025-09-13 16:32:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Small JS for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const id = a.getAttribute('href').slice(1);
+    const el = document.getElementById(id);
+    if(el){
+      e.preventDefault();
+      el.scrollIntoView({behavior:'smooth', block:'start'});
+    }
+  })
+});
